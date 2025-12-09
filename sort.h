9012c4547ef42498e87174b21fd471e3902b8672
laradreamer79 <laradreamer79@gmail.com>
2025-12-09 09:42:02 +0000

    Add 0-bubble_sort.c and Big O notation file

---
+++ sort.h
@@ -0,0 +1,24 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * For future tasks (they require linked list sorting)
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+void print_array(const int *array, size_t size);
+
+void bubble_sort(int *array, size_t size);
+
+#endif /* SORT_H */
+

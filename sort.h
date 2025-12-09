2e85a0fcdc006bbbe707ec055324200b03b67d0d
laradreamer79 <laradreamer79@gmail.com>
2025-12-09 09:51:34 +0000

    fix betty style for sort.h and fix 0-O code

--- sort.h
+++ sort.h
@@ -5,19 +5,19 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s - Doubly linked list node
- *
- * For future tasks (they require linked list sorting)
+ * struct listint_s - Doubly linked list node structure
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element
+ * @next: Pointer to the next element
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
-
 void bubble_sort(int *array, size_t size);
 
 #endif /* SORT_H */

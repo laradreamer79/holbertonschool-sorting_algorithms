a54d7c30027466d7a7214a040194d42391d81025
root <alfaidi.afnan@gmail.com>
2025-12-16 13:12:58 +0000

    add task 2: function that sorts an array of integers in ascending order using the Selection sort algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,39 +1,39 @@
 #include "sort.h"
 
 /**
- * selection_sort - Sorts an array of integers in ascending order
- *                  using the Selection sort algorithm
- * @array: The array to be sorted
- * @size: Number of elements in @array
- */
+* selection_sort - Sorts an array of integers in ascending order
+*                  using the Selection sort algorithm
+* @array: The array to be sorted
+* @size: Number of elements in @array
+*/
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_idx;
-    int temp;
+size_t i, j, min_idx;
+int temp;
 
-    if (array == NULL || size < 2)
-        return;
+if (array == NULL || size < 2)
+return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min_idx = i;
-        
-        /* Find the minimum element in unsorted array */
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_idx])
-                min_idx = j;
-        }
-        
-        /* Swap the found minimum element with the first element */
-        if (min_idx != i)
-        {
-            temp = array[i];
-            array[i] = array[min_idx];
-            array[min_idx] = temp;
-            
-            /* Print array after each swap */
-            print_array(array, size);
-        }
-    }
+for (i = 0; i < size - 1; i++)
+{
+min_idx = i;
+
+/* Find the minimum element in unsorted array */
+for (j = i + 1; j < size; j++)
+{
+if (array[j] < array[min_idx])
+min_idx = j;
+}
+
+/* Swap the found minimum element with the first element */
+if (min_idx != i)
+{
+temp = array[i];
+array[i] = array[min_idx];
+array[min_idx] = temp;
+
+/* Print array after each swap */
+print_array(array, size);
+}
+}
 }
